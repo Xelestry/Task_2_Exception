bd9e54d7a3f4bf7e812918fbb5e25a01a7898198
Kirill Mityushchenko <Kirill_Mityushchenko@epam.com>
2020-07-20 19:27:15 +0600

    done

---
+++ Task_2_Exception_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Task_2_Exception_2;
+
+namespace Task_2_Exception_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make true for skip Task_2_Exception_1
+            bool closeProgram = true;
+
+            Console.WriteLine("Number of lines: ");
+
+            while (!closeProgram)
+            {
+                if (int.TryParse(Console.ReadLine(), out int numberOfLines))
+                {
+                    List<string> lines = new List<string>();
+
+                    for (int i = 0; i < numberOfLines; i++)
+                    {
+                        Console.WriteLine($"Enter line number {i + 1}: ");
+                        lines.Add(Console.ReadLine());
+                    }
+
+                    Console.WriteLine("__________________________________________________________\n\n");
+
+                    for (int i = 0; i < numberOfLines; i++)
+                    {
+                        string lineWithoutSpaces = lines.ElementAt(i).Replace(" ", "");
+
+                        if (lineWithoutSpaces.Length > 0)
+                        {
+                            char firstLetter = lineWithoutSpaces[0];
+                            Console.WriteLine($"First letter of line {i + 1}: {firstLetter}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Line {i + 1} is empty! :c");
+                        }
+                    }
+
+                    closeProgram = true;
+                }
+                else
+                {
+                    Console.WriteLine("It's not a number! Please, try again: ");
+                }
+            }
+
+            // remove comment for Testing Task_2_Exception_2 library
+            MyParser.StringToInt("    ", out int targetNumber);
+        }
+    }
+}
---
+++ Task_2_Exception_2/MyParser.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Task_2_Exception_2
+{
+    public static class MyParser
+    {
+        const int ZeroSymbol = 49;
+        const int NineSymbol = 57;
+
+        public static bool StringToInt(string sourceString, out int targetInt)
+        {
+            targetInt = 0;
+
+            if (IsSourceStringValid(sourceString))
+            {
+                int? resultNumber = ConverStringToInt(sourceString);
+
+                if (resultNumber.HasValue)
+                {
+                    targetInt = resultNumber.Value;
+                    return true;
+                }
+
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static int? ConverStringToInt(string sourceString)
+        {
+            int? resultNumber = 0;
+            const int FormulaConst = 10;
+
+            for (int i = 0; i < sourceString.Length; i++)
+            {
+                int? currentNumeral = CharToInt(sourceString[i]);
+
+                if (currentNumeral != null)
+                {
+                    resultNumber = resultNumber * FormulaConst + currentNumeral;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return resultNumber;
+        }
+
+        private static bool IsSourceStringValid(string sourceString)
+        {
+            if (sourceString == string.Empty)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < sourceString.Length; i++)
+            {
+                if (sourceString[i] < ZeroSymbol || sourceString[i] > NineSymbol)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int? CharToInt(char letter)
+        {
+            switch (letter)
+            {
+                case '0':
+                    return 0;
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                default:
+                    return null;
+            }
+        }
+    }
+}
